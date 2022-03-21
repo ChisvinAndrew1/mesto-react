59b486842ea8c084b8067e7928fa5f87a449c93f
ChisvinAndrew1 <frolik947@gmail.com>
2022-03-21 10:29:02 +0300

    hotfix

--- mesto-react/src/components/Maining/Main.js
+++ mesto-react/src/components/Maining/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "../Card/Card.js";
+import Card from "../Carding/Card.js";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 function Main({
